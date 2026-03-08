e3f600772038a3f1dd81a22171ce13371faa931a
Richard DJE <richard.dje@gmail.com>
2026-03-08 17:46:55 +0100

    Expand SV realistic corpus to version 25

---
+++ rust/test_data/grammar_quality/systemverilog_nexsim_realistic_corpus/preprocess_deep_include_package_width_fourteen_child_defs.svh
@@ -0,0 +1,8 @@
+package deep_cfg_pkg;
+  localparam logic [3:0] A = 4'b1010;
+  localparam logic [3:0] B = 4'b0101;
+endpackage
+
+module deep_width_pipe_stage(input logic [3:0] a, input logic [3:0] b, output logic [3:0] y);
+  assign y = a;
+endmodule
